fe4f4eda765f0cde297af16ab65348a66925219d
Suguru Iwata <sgr198931@gmail.com>
2025-02-18 20:28:07 +0900

    cors設定

---
+++ app/Http/Middleware/HandleCors.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HandleCors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')  // 任意のオリジンを設定
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
+    }
+}
